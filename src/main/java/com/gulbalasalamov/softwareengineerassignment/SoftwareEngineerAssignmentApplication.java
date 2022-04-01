1097f849d01ff188ea2ca928f1749b8b98f54ddc
gulbalasalamov <g.salamov@gmail.com>
2022-04-01 22:07:16 +0300

    Initial commit. Project created. Dependencies added

---
+++ src/main/java/com/gulbalasalamov/softwareengineerassignment/SoftwareEngineerAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.gulbalasalamov.softwareengineerassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareEngineerAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareEngineerAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gulbalasalamov/softwareengineerassignment/SoftwareEngineerAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gulbalasalamov.softwareengineerassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareEngineerAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
